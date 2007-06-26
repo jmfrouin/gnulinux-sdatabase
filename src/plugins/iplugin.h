eebf726af52a4d1b9db274bc7d8cd2505eea8858
snoogie_sf <snoogie_sf@8b1006bb-1933-0410-8054-d6cb1d15217c>
2007-06-26 13:16:37 +0000

    Adding header + svn:keywords
    git-svn-id: svn://svn.code.sf.net/p/sdatabase/code@23 8b1006bb-1933-0410-8054-d6cb1d15217c

--- src/plugins/iplugin.h
+++ src/plugins/iplugin.h
@@ -1,14 +1,36 @@
+/*
+-------------------------------------------------------------------------
+Project : sdatabase
+-------------------------------------------------------------------------
+Creator : Frouin Jean-Michel
+-------------------------------------------------------------------------
+$Date$
+$Rev$
+$Author$
+-------------------------------------------------------------------------
+This program is free software; you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation; version 2 of the License.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+-------------------------------------------------------------------------
+*/
+
 #ifndef _IPLUGIN_H_
 #define _IPLUGIN_H_
 
 #include <config.h>
 #include <string>
 #include <tools/name.h>
+#include <files/ifile.h>
 
 /**
  * @brief Plugin Interface
  */
-class IPlugin : public CName
+class IPlugin : public CName, IFile
 {
 	public:
 		/*!
