ba3a85c96a463af6e5a32f858e2ba4647294fbda
snoogie_sf <snoogie_sf@8b1006bb-1933-0410-8054-d6cb1d15217c>
2007-08-09 14:33:51 +0000

    savanah
    git-svn-id: svn://svn.code.sf.net/p/sdatabase/code@30 8b1006bb-1933-0410-8054-d6cb1d15217c

--- archive.sh
+++ archive.sh
@@ -1,16 +1,1 @@
-#!/bin/bash
-
-echo "We clean project ..."
-make clean
-rm *.bz2
-echo "We tar ..."
-tar cvf sdatabase_`date +%d%m%y_%H%M`.tar *
-echo "We compress ..."
-bzip2 sdatabase_`date +%d%m%y_%H%M`.tar 
-echo "Archivage sur OceaneDS..."
-scp sdatabase_`date +%d%m%y_%H%M`.tar.bz2 192.168.1.102:~/backup/sdatabase/ 
-#echo "Archivage sur Thanos..."
-#scp sdatabase_`date +%d%m%y_%H%M`.tar.bz2 192.168.1.100:~/backup/ 
-#echo "Archivage sur Dell de Laure..."
-#scp sdatabase_`date +%d%m%y_%H%M`.tar.bz2 192.168.1.101:~/backup/ 
-echo "Fini ..."
+00 * 27 Jun * /home/snoogie/sdatabase/archive.sh
